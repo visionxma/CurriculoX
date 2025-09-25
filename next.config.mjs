1e19d03d072e0f09b312c7d06019c664b65a83f9
VisionX <visionxma@gmail.com>
2025-09-25 10:07:51 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Adicione as linhas abaixo:
+  basePath: '/CurriculoX',
+  assetPrefix: '/CurriculoX/',
 }
 
 export default nextConfig
